1f680c67adfd1cc126c08437c1a730e2ea21002b
Deadzoke <guilhermejardim11@gmail.com>
2023-06-27 17:02:25 +0100

    Enhanced about section

--- src/components/About/Profile.js
+++ src/components/About/Profile.js
@@ -1,35 +1,16 @@
-import styles from "./Profile.module.scss";
+import styles from './Profile.module.scss';
 
-import background from "../../assets/light/images/profile-background.jpg";
+import avatar from '../../assets/light/images/avatar.jpg';
 
 const Profile = () => {
-    const today = new Date().getTime();
-    const birthday = new Date("1996-08-31").getTime();
-    const age = Math.abs(new Date(today - birthday).getUTCFullYear() - 1970);
-
     return (
-        <div
-            className={styles.profile}
-            style={{ backgroundImage: `url("${background}")` }}
-        >
-            <div>
-                <h3>Name</h3>
-                <h4>
-                    Guilherme
-                    <br />
-                    Jardim
-                </h4>
-            </div>
-
-            <div>
-                <h3>Age</h3>
-                <h4>{age} yrs</h4>
-            </div>
-
-            <div>
-                <h3>Nationality</h3>
-                <h4>Portuguese</h4>
-            </div>
+        <div className={styles.profile}>
+            <img src={avatar} alt='Guilherme Jardim' />
+            <p>
+                Hi there! I'm Guilherme Jardim and I'm a UX/UI Designer from
+                Portugal, and I have the goal to improve the quality of my
+                projects and broaden my skillset!
+            </p>
         </div>
     );
 };
--- src/components/Section/AboutSection.js
+++ src/components/Section/AboutSection.js
@@ -1,20 +1,19 @@
-import About from "../About/Profile";
-import Experience from "../About/Experience";
-import Dot from "../UI/Dot";
-import Section from "./Section";
-import SectionTitle from "./SectionTitle";
+import Profile from '../About/Profile';
+import Experience from '../About/Experience';
+import Dot from '../UI/Dot';
+import Section from './Section';
+import SectionTitle from './SectionTitle';
 
 const AboutSection = () => {
     return (
         <Section background pattern>
-            <SectionTitle title="About" to="/portfolio/about" text="Know More" />
-            <About />
-            <p>
-                Hello! I'm Guilherme Jardim and I'm a UX/UI Designer,
-                experienced in Figma, Adobe Creative Suite, React.js, HTML,
-                SCSS, JavaScript.
-            </p>
-            <Dot style={{ marginTop: "38px", marginBottom: "24px" }} />
+            <SectionTitle
+                title='About'
+                to='/portfolio/about'
+                text='Know More'
+            />
+            <Profile />
+            <Dot style={{ marginTop: '38px', marginBottom: '24px' }} />
             <Experience />
         </Section>
     );
