ab0051e040affa4b304657fee6cfa0bf85d0b817
Deadzoke <guilhermejardim11@gmail.com>
2023-07-28 14:35:48 +0100

    Enhanced transition

--- src/components/Animations/Transition.js
+++ src/components/Animations/Transition.js
@@ -4,49 +4,57 @@ import styles from './Transition.module.scss';
 
 import Logo from '../UI/Logo';
 
+const divVariants = {
+	visible: {
+		left: '100vw',
+		transitionEnd: { left: '-100vw' },
+	},
+	hidden: {
+		left: 0,
+	},
+	transition: {
+		duration: 0.7,
+		ease: [0.87, 0, 0.13, 1],
+	},
+};
+
+const logoVariants = {
+	visible: {
+		x: 0,
+	},
+	hidden: {
+		x: '-200%',
+		transitionEnd: { x: '200%' },
+	},
+	transition: {
+		duration: 0.7,
+		ease: [0.87, 0, 0.13, 1],
+	},
+};
+
 const Transition = () => {
 	return (
-		<>
-			<motion.div
-				className={styles.slide_in}
-				key='slide_in'
-				initial={{ scaleX: 0 }}
-				animate={{ scaleX: 0 }}
-				exit={{ scaleX: 1 }}
-				transition={{ duration: 0.7, ease: [0.87, 0, 0.13, 1] }}
-			/>
-
-			<motion.div
-				className={styles.slide_out}
-				key='slide_out'
-				initial={{ scaleX: 1 }}
-				animate={{ scaleX: 0 }}
-				exit={{ scaleX: 0 }}
-				transition={{ duration: 0.7, ease: [0.87, 0, 0.13, 1] }}
-			/>
-
+		<motion.div
+			className={styles.slide}
+			key='slide'
+			variants={divVariants}
+			initial='hidden'
+			animate='visible'
+			exit='hidden'
+			transition={{ duration: 0.7, ease: [0.87, 0, 0.13, 1] }}
+		>
 			<motion.div
 				className={styles.logo}
-				key='slide_in__logo'
-				initial={{ x: '-65vw' }}
-				animate={{ x: '-65vw' }}
-				exit={{ x: 0 }}
+				key='logo'
+				variants={logoVariants}
+				initial='visible'
+				animate='hidden'
+				exit='visible'
 				transition={{ duration: 0.7, ease: [0.87, 0, 0.13, 1] }}
 			>
 				<Logo />
 			</motion.div>
-
-			<motion.div
-				className={styles.logo}
-				key='slide_out__logo'
-				initial={{ x: 0 }}
-				animate={{ x: '65vw' }}
-				exit={{ x: '65vw' }}
-				transition={{ duration: 0.7, ease: [0.87, 0, 0.13, 1] }}
-			>
-				<Logo />
-			</motion.div>
-		</>
+		</motion.div>
 	);
 };
 
