c16012b779614c3c01ab4917f7e0bbac736759b5
Deadzoke <guilhermejardim11@gmail.com>
2023-08-09 16:30:43 +0100

    Desktop improvements

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,12 +1,13 @@
 import styles from './Section.module.scss';
 
 const Section = (props) => {
-	const classes = `${styles.section} ${props.className} ${
-		props.background ? styles.background : ''
-	} ${props.extend ? styles.extend : ''}`;
+	const classes = `${styles.section} ${props.className ? props.className : ''} ${props.background ? styles.background : ''} ${props.extend ? styles.extend : ''}`;
 
 	return (
-		<section id={props.id} className={classes}>
+		<section
+			id={props.id}
+			className={classes}
+		>
 			{props.children}
 		</section>
 	);
