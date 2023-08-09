9ab83a318936ce064d8d564dee3132685016c52a
Deadzoke <guilhermejardim11@gmail.com>
2023-08-09 11:00:02 +0100

    Gallery desktop development

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,18 +1,63 @@
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 import styles from './Gallery.module.scss';
 
+import typography from '../../assets/icons/typography.svg';
+import iconography from '../../assets/icons/iconography.svg';
+import palette from '../../assets/icons/palette.svg';
+import branding from '../../assets/icons/branding.svg';
+
+const icons = {
+	typography: typography,
+	iconography: iconography,
+	palette: palette,
+	branding: branding,
+};
+
 const Gallery = (props) => {
+	const [t] = useTranslation();
+
 	return (
-		<ul className={styles.gallery}>
+		<div className={styles.gallery}>
 			{props.items.map((item) => (
-				<li key={item.id}>
+				<div key={item.id}>
 					<Link to={`${props.linkPrefix}${item.id}`}>
-						<img src={item.thumbnail} alt={item.title} />
+						<img
+							src={item.thumbnail}
+							alt={item.title}
+						/>
+						<div className={styles.content}>
+							<div>
+								<div>
+									<h4>{item.title}</h4>
+									<span>
+										{item.categories.map((category, index) => {
+											if (index === 0) return t(category);
+											if (index === item.categories.length - 1) return ` & ${t(category)}`;
+											return `, ${t(category)}`;
+										})}
+									</span>
+								</div>
+								<div className={styles.tags}>
+									{item.tags.map(
+										(tag) =>
+											icons[tag] && (
+												<img
+													key={tag}
+													src={icons[tag]}
+													alt={tag}
+													className='icon_white'
+												/>
+											)
+									)}
+								</div>
+							</div>
+						</div>
 					</Link>
-				</li>
+				</div>
 			))}
-		</ul>
+		</div>
 	);
 };
 
